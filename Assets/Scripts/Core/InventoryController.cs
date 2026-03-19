0a49535bc137ae299d4783aa312f90a6468d0387
Gerardo <gerardogonzalezmartin2004@gmail>
2026-03-19 13:27:42 +0100

    Revert "Merge branch 'Dani_Develop'"
    This reverts commit ef3ab5c67fc4fa020c9ee1cbf32ee4866e5e9e5e, reversing
    changes made to 01a2ae34cd729ad79344fa343e0708759a615b61.

---
+++ Assets/Scripts/Core/InventoryController.cs
@@ -0,0 +1,20 @@
+using AbyssalReach.Data;
+using NUnit.Framework;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    public List itemList = new List();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
