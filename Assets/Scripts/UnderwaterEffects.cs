5271dd7677851fb9d5d6ce543e88b4eae5192b68
DanielFT <danielferrert10@gmail.com>
2026-04-18 15:05:48 +0200

    inicio visuals bajo el agua

---
+++ Assets/Scripts/UnderwaterEffects.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class UnderwaterEffects : MonoBehaviour
+{
+    [SerializeField] private GameObject waterFx;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        waterFx.SetActive(true);
+        RenderSettings.fog = true;
+        Debug.Log("Debería activarse la fog");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        waterFx.SetActive(false);
+        RenderSettings.fog = false;
+        Debug.Log("Debería desactivarse la fog");
+    }
+}
