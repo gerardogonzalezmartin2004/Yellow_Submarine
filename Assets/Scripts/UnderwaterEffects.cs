9a69d66dd140d048c4cadc6cb56c826e53d13e49
DanielFT <danielferrert10@gmail.com>
2026-05-21 17:30:50 +0200

    Burbujas guays (tambien al entrar al buceo)

--- Assets/Scripts/UnderwaterEffects.cs
+++ Assets/Scripts/UnderwaterEffects.cs
@@ -1,15 +1,25 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class UnderwaterEffects : MonoBehaviour
 {
     [SerializeField] private GameObject WaterArriba;
     [SerializeField] private GameObject WaterAbajo;
+    [SerializeField] private ParticleSystem bubblesEffect;
+
+    private void Start()
+    {
+        RenderSettings.fog = false;
+        WaterArriba.SetActive(true);
+        WaterAbajo.SetActive(false);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         RenderSettings.fog = true;
         WaterArriba.SetActive(false);
         WaterAbajo.SetActive(true);
+        bubblesEffect.Play();
         Debug.Log("Debería activarse la fog");
     }
 
@@ -18,6 +28,8 @@ public class UnderwaterEffects : MonoBehaviour
         RenderSettings.fog = false;
         WaterArriba.SetActive(true);
         WaterAbajo.SetActive(false);
+        bubblesEffect.Stop();
+        bubblesEffect.DORestart();
         Debug.Log("Debería desactivarse la fog");
     }
 }
